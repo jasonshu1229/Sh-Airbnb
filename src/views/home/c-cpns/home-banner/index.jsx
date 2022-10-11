0dfd4d9b9133f1f535a768aa85d29b9ca5ad81ef
jasonshu1229 <shuhao1229@163.com>
2022-10-11 13:24:02 +0800

    feat: 插入首页大屏图

---
+++ src/views/home/c-cpns/home-banner/index.jsx
@@ -0,0 +1,11 @@
+import React, {memo} from 'react';
+import {BannerWrapper} from "@/views/home/c-cpns/home-banner/style";
+
+const HomeBanner = memo(() => {
+  return (
+    <BannerWrapper>
+    </BannerWrapper>
+  );
+});
+
+export default HomeBanner;
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,10 +1,12 @@
 import React, { memo } from 'react'
+import {HomeWrapper} from "@/views/home/style";
+import HomeBanner from "@/views/home/c-cpns/home-banner";
 
 const Home = memo(() => {
   return (
-    <div>
-      Home
-    </div>
+    <HomeWrapper>
+      <HomeBanner />
+    </HomeWrapper>
   )
 })
 
