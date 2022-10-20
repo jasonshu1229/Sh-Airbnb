67784a429822819a01efda592dc5419be4f4915e
jasonshu1229 <shuhao1229@163.com>
2022-10-20 09:56:34 +0800

    fix：指示器newIndex修复

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -1,4 +1,4 @@
-import React, {memo, useRef} from 'react';
+import React, {memo, useRef, useState} from 'react';
 import PropTypes from 'prop-types';
 import { Carousel } from 'antd';
 
@@ -6,14 +6,24 @@ import {ItemWrapper} from "@/components/room-item/style";
 import Rating from "@mui/material/Rating";
 import IconArrowLeft from "@/assets/svg/icon-arrow-left";
 import IconArrowRight from "@/assets/svg/icon-arrow-right";
+import Indicator from "@/base-ui/indicator";
+import classNames from "classnames";
 
 const RoomItem = memo((props) => {
   const { itemData, itemWidth = "25%" } = props;
+  const [selectIndex, setSelectIndex] = useState(0);
   const slideRef = useRef();
   
   const controlClickHandle = (isRight = true) => {
     // 上一个面板/下一个面板
     isRight ? slideRef.current.next() : slideRef.current.prev();
+    
+    // 最新的索引
+    let newIndex = isRight ? selectIndex + 1 :selectIndex - 1;
+    const length = itemData.picture_urls.length;
+    if (newIndex < 0) newIndex = length - 1
+    if (newIndex > length - 1) newIndex = 0
+    setSelectIndex(newIndex);
   }
   
   return (
@@ -33,6 +43,19 @@ const RoomItem = memo((props) => {
             <div className='btn right' onClick={e => controlClickHandle(true)}>
               <IconArrowRight width={30} height={30} />
             </div>
+          </div>
+          <div className='indicator'>
+            <Indicator selectIndex={selectIndex}>
+              {
+                itemData.picture_urls?.map((item, index) => {
+                  return (
+                    <div className='item' key={item}>
+                      <span className={classNames("dot", { active: selectIndex === index})} />
+                    </div>
+                  )
+                })
+              }
+            </Indicator>
           </div>
           <Carousel dots={false} ref={slideRef}>
             {
--- src/components/room-item/style.js
+++ src/components/room-item/style.js
@@ -44,6 +44,36 @@ export const ItemWrapper = styled.div`
         }
       }
     }
+  
+    .indicator {
+      position: absolute;
+      z-index: 9;
+      bottom: 10px;
+      left: 0;
+      right: 0;
+      width: 30%;
+      margin: 0 auto;
+    
+      .item {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 14.29%;
+      
+        .dot {
+          width: 6px;
+          height: 6px;
+          background-color: #fff;
+          border-radius: 50%;
+        
+          &.active {
+            width: 9px;
+            height: 9px;
+            background-color: red;
+          }
+        }
+      }
+    }
   }
   
   .cover {
