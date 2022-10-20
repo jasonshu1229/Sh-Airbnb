48033aafcfc978aca751b2fba270da9504686bc9
jasonshu1229 <shuhao1229@163.com>
2022-10-20 18:50:08 +0800

    fix：Logo跳转到主页和分解组件

--- src/components/app-header/c-cpns/header-left/index.jsx
+++ src/components/app-header/c-cpns/header-left/index.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {memo} from 'react';
 import {LeftWrapper} from "@/components/app-header/c-cpns/header-left/style";
 import IconLogo from "@/assets/svg/icon_logo";
+import {useNavigate} from "react-router-dom";
 
-const HeaderLeft = () => {
+const HeaderLeft = memo((props) => {
+  
+  const navigate = useNavigate();
+  const logoClickHandle = () => {
+    navigate("/home");
+  }
+  
   return (
     <LeftWrapper>
-      <div className='logo'>
+      <div className='logo' onClick={logoClickHandle}>
         <IconLogo />
       </div>
     </LeftWrapper>
   );
-};
+});
 
 export default HeaderLeft;
--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -26,49 +26,56 @@ const RoomItem = memo((props) => {
     setSelectIndex(newIndex);
   }
   
+  const pictureElement =(
+    <div className='cover'>
+      <img src={itemData.picture_url} alt="" />
+    </div>
+  )
+  
+  const sliderElement = (
+    <div className='slider'>
+      <div className='control'>
+        <div className='btn left' onClick={e => controlClickHandle(false)}>
+          <IconArrowLeft width={30} height={30} />
+        </div>
+        <div className='btn right' onClick={e => controlClickHandle(true)}>
+          <IconArrowRight width={30} height={30} />
+        </div>
+      </div>
+      <div className='indicator'>
+        <Indicator selectIndex={selectIndex}>
+          {
+            itemData.picture_urls?.map((item, index) => {
+              return (
+                <div className='item' key={item}>
+                  <span className={classNames("dot", { active: selectIndex === index})} />
+                </div>
+              )
+            })
+          }
+        </Indicator>
+      </div>
+      <Carousel dots={false} ref={slideRef}>
+        {
+          itemData?.picture_urls?.map(item => {
+            return (
+              <div className='cover' key={item}>
+                <img src={item} alt="" />
+              </div>
+            )
+          })
+        }
+      </Carousel>
+    </div>
+  )
+  
   return (
     <ItemWrapper
       verifyColor={itemData?.verify_info?.text_color || "#39576a"}
       itemWidth={itemWidth}
     >
       <div className='inner'>
-        {/*<div className='cover'>*/}
-        {/*  <img src={itemData.picture_url} alt="" />*/}
-        {/*</div>*/}
-        <div className='slider'>
-          <div className='control'>
-            <div className='btn left' onClick={e => controlClickHandle(false)}>
-              <IconArrowLeft width={30} height={30} />
-            </div>
-            <div className='btn right' onClick={e => controlClickHandle(true)}>
-              <IconArrowRight width={30} height={30} />
-            </div>
-          </div>
-          <div className='indicator'>
-            <Indicator selectIndex={selectIndex}>
-              {
-                itemData.picture_urls?.map((item, index) => {
-                  return (
-                    <div className='item' key={item}>
-                      <span className={classNames("dot", { active: selectIndex === index})} />
-                    </div>
-                  )
-                })
-              }
-            </Indicator>
-          </div>
-          <Carousel dots={false} ref={slideRef}>
-            {
-              itemData?.picture_urls?.map(item => {
-                return (
-                  <div className='cover' key={item}>
-                    <img src={item} alt="" />
-                  </div>
-                )
-              })
-            }
-          </Carousel>
-        </div>
+        { !itemData.picture_urls ? pictureElement : sliderElement }
         <div className='desc'>
           {itemData.verify_info.messages.join(" · ")}
         </div>
