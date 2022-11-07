3e8acebada5d549a819144af2ab7af4cbf4e374e
jasonshu1229 <shuhao1229@163.com>
2022-11-07 12:39:20 +0800

    feat：页面切换滚动到顶部useScrollTop的封装

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React, {memo} from 'react';
-import { useRoutes } from 'react-router-dom';
+import React, {memo, useEffect} from 'react';
+import {useLocation, useRoutes} from 'react-router-dom';
 
 import routes from "@/router";
 import AppHeader from "@/components/app-header";
 import AppFooter from "@/components/app-footer";
+import {useScrollTop} from "@/hooks";
 
 const App = memo(() => {
+  useScrollTop();
+  
   return (
     <div className='app'>
       <AppHeader />
